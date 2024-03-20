7a963faf6d7d777081575c6610e97cc53accfd2e
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-03-20 13:55:34 -0800

    back to mode: 'cors' because same origin was not working

--- src/lib/api.js
+++ src/lib/api.js
@@ -103,7 +103,7 @@ function getToken() {
 }
 
 async function handleFetch(endpoint, options = {}, routeName, textResponse) {
-    options.mode = 'same-origin';
+    options.mode = 'cors';
 
     const res = await fetch(
         ROOT + endpoint, options,
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
         try {
             let adminRes = await axios("https://api.vtscases.com/admin/signIn", {
                 method: 'POST',
-                mode: 'same-origin',
+                mode: 'cors',
                 headers: {
                     "email": formData.email,
                     "password": formData.password,
