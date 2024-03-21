71dbbd418741f4a85b8bdd62d49d29176f013d19
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-03-21 03:40:49 -0800

    chnaged the middle product in home page to a clcored case instead of printed

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     const [products, setProducts] = useState([])
     useEffect(()=>{
-        getProductsByIds([15, 24,33])
+        getProductsByIds([15, 17,33])
             .then(p => {
                 setProducts(p)
             })
