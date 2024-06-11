d5b3db0d359d1822a01de26f421ed3df44336a5c
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-06-11 11:15:37 -0800

    tryng to redeploy  and connect with new images its not working

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,72 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import ProductModal from "./ProductModal";
 
-import Iphone14ProMaxPlainCaseImage from "../ProductImages/iphone14ProMaxPlainCaseSlicone.jpg";
-import Iphone15PlainCover from "../ProductImages/iphone15PlainCaseSilicone.jpg";
-import siliconeSamuraiChnieaseLettersIphone14ProMax from "../ProductImages/iphone14ProMaxSamuraiChineaseLetters.jpg";
+import Iphone14ProMaxPlainCaseImage from "../ProductImages/iphone 14 pro max plain case slicone.jpg";
+import Iphone15PlainCover from "../ProductImages/iphone 15 plain case silicone.jpg";
+import premiumRubberPinkLotus from "../ProductImages/a14 premium rubber case pink lotus flower.jpg";
 import iphone14proMaxPrint2 from "../ProductImages/iphone14proMaxPrint2.webp";
 import iphone14proMaxPrint3 from "../ProductImages/iphone14proMaxPrint3.webp";
 import iphone14proMaxBlue from "../ProductImages/iphone14proMaxBlue.jpg";
 import iphone14proMaxBlack from "../ProductImages/iphone14proMaxBlack.jpg";
-import iphone15Black from "../ProductImages/iphone15Black.webp";
-import iphone14proMaxWhite from "../ProductImages/iphone14proMaxWhite.jpg";
-import iphone15White from "../ProductImages/iphone15white.webp";
-import iphone15blue from "../ProductImages/iphone15Blue.webp";
+import iphone15Black from "../ProductImages/iphone15Black.webp"
+import iphone14proMaxWhite from "../ProductImages/iphone14proMaxWhite.jpg"
+import iphone15White from "../ProductImages/iphone15white.webp"
+import iphone15blue from "../ProductImages/iphone15Blue.webp"
 
-import iphone15print1 from "../ProductImages/iphone15print3.jpeg";
-import iphone15print2 from "../ProductImages/iphone15print2.jpeg";
-import iphone15print3 from "../ProductImages/iphone15print1.jpeg";
+import iphone15print1 from "../ProductImages/iphone15print3.jpeg"
+import iphone15print2 from "../ProductImages/iphone15print2.jpeg"
+import iphone15print3 from "../ProductImages/iphone15print1.jpeg"
 
-import s23plain from "../ProductImages/s23plain.webp";
-import s23Black from "../ProductImages/s23Black.jpeg";
-import s23white from "../ProductImages/s23White.jpeg";
-import s23blue from "../ProductImages/s23blue.jpeg";
+import s23plain from "../ProductImages/s23plain.webp"
+import s23Black from "../ProductImages/s23Black.jpeg"
+import s23white from "../ProductImages/s23White.jpeg"
+import s23blue from "../ProductImages/s23blue.jpeg"
 
-import s23print1 from "../ProductImages/s23print1.jpg";
-import s23print2 from "../ProductImages/s23print2.jpeg";
-import s23print3 from "../ProductImages/s23print3.jpeg";
+import s23print1 from "../ProductImages/s23print1.jpg"
+import s23print2 from "../ProductImages/s23print2.jpeg"
+import s23print3 from "../ProductImages/s23print3.jpeg"
 
-import rubberCaseiphone14 from "../ProductImages/iphone pro max rubber.jpg";
+import rubberCaseS23 from "../ProductImages/rubbercaseprint.jpg"
 
-export default function ProductCard({ p, isSoldOut }) {
+
+
+
+
+export default function ProductCard({p, isSoldOut}) {
   const [imageHeight, setImageHeight] = useState('300px'); // Default height for larger screens
+  console.log("vikasproducts", parseFloat)
   const [image, setImage] = useState("");
   const [modalShown, setModalShown] = useState(false);
 
   // Map product IDs to local storage image paths
   const imageMapping = {
-    1: Iphone14ProMaxPlainCaseImage, // Use the actual product ID here
-    2: Iphone15PlainCover, // Use the actual product ID here
-    3: siliconeSamuraiChnieaseLettersIphone14ProMax,
-    15: iphone14proMaxPrint2,
-    16: iphone14proMaxPrint3,
-    17: iphone14proMaxBlue,
-    18: iphone14proMaxBlack,
-    19: iphone14proMaxWhite,
-    20: iphone15Black,
-    21: iphone15White,
-    22: iphone15blue,
-    23: iphone15print1,
-    24: iphone15print2,
-    25: iphone15print3,
-    34: s23print1,
-    35: s23print2,
-    36: s23print3,
-    37: s23blue,
-    38: s23Black,
-    39: s23white,
-    33: s23plain,
-    41: rubberCaseiphone14
+    6: Iphone14ProMaxPlainCaseImage, // Use the actual product ID here
+    7: Iphone15PlainCover, // Use the actual product ID here
+    8: premiumRubberPinkLotus,
+    8: iphone14proMaxPrint2,
+    9: iphone14proMaxPrint3,
+    10: iphone14proMaxBlue,
+    11: iphone14proMaxBlack,
+    12: iphone14proMaxWhite,
+    16:iphone15Black,
+    18:iphone15White,
+    29:iphone15blue,
+    20:iphone15print1,
+    21:iphone15print2,
+    22:iphone15print3,
+    23:s23print1,
+    24:s23print2,
+    25:s23print3,
+    27:s23blue,
+    28:s23Black,
+    39:s23white,
+    26:s23plain,
+    30:rubberCaseS23
+
     // Add more entries as needed
   };
 
   // Set image based on product ID
-  useEffect(() => {
-    const newImage = imageMapping[p.productId];
-    setImage(newImage);
-  }, [p.productId]);
+  const newImage = imageMapping[p.productId];
+  console.log(p.productId, " p.productId", "newImage", newImage, "image", image)
 
-  const updatedProduct = { ...p, image };
+  if (newImage !== image) {
+    setImage(newImage);
+  }
+  // const updatedProduct = Object.assign({}, p, { image: newImage });
+  const updatedProduct = { ...p, image: newImage };
   const displayPrice = p.productPrice.toFixed(2);
 
   const handleClick = () => {
@@ -79,87 +87,95 @@ export default function ProductCard({ p, isSoldOut }) {
     setModalShown(false);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 600) {
-        setImageHeight('200px'); // Set height to 200px for mobile screens
-      } else {
-        setImageHeight('300px'); // Set height to 300px for larger screens
-      }
-    };
+  console.log("Rendering with Image:", image);
 
-    window.addEventListener('resize', handleResize);
-    handleResize();
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+   
+useEffect(() => {
+  const handleResize = () => {
+    if (window.innerWidth <= 600) {
+      setImageHeight('200px'); // Set height to 200px for mobile screens
+    } else {
+      setImageHeight('300px'); // Set height to 300px for larger screens
+    }
+  };
 
-  return (
-    <>
-      <button 
-        className={`product-card ${isSoldOut ? 'sold-out' : ''}`} 
-        onClick={handleClick} 
-        disabled={isSoldOut}
-        style={{ 
-          width: '100%', 
-          height: 'auto', 
-          padding: '0', 
-          display: 'flex', 
-          flexDirection: 'column', 
-          justifyContent: 'space-between'
-        }}
-      >
-        {image && (
-          <img 
-            src={image} 
-            alt={image} 
-            style={{ 
-              width: '100%', 
-              height: imageHeight, 
-              objectFit: 'cover', 
-              borderRadius: '0.5rem',
-              display: 'block',
-              opacity: isSoldOut ? '0.5' : '1'  // Dim the image for sold out products
-            }} 
-          />
-        )}
-        <div className="info">
-          <h3>{p.productName}</h3>
-          <div className="price">
-            {isSoldOut ? (
-              <div>
-                {['14', '15', '16', '23', '24', '25', '34', '35', '36'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK120</span>
-                )}
-                {['20', '21', '22', '17', '18', '19', '37', '38', '39'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK110</span>
-                )}
-                {['12', '13', '33'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK100</span>
-                )}
-                {' '}SEK{displayPrice} (Sold Out)
-              </div>
-            ) : (
-              <div>
-                {['14', '15', '16', '23', '24', '25', '34', '35', '36'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK120</span>
-                )}
-                {['20', '21', '22', '17', '18', '19', '37', '38', '39'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK110</span>
-                )}
-                {['12', '13', '33'].includes(p.productId.toString()) && (
-                  <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK100</span>
-                )}
-                {' '}SEK{displayPrice}
-              </div>
-            )}
-          </div>
+  window.addEventListener('resize', handleResize);
+  handleResize();
+
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
+return (
+  <>
+    <button 
+      className={`product-card ${isSoldOut ? 'sold-out' : ''}`} 
+      onClick={handleClick} 
+      disabled={isSoldOut}
+      style={{ 
+        width: '100%', 
+        height: 'auto', 
+        padding: '0', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'space-between'
+      }}
+    >
+      {image && (
+        <img 
+          src={image} 
+          alt={image} 
+          style={{ 
+            width: '100%', 
+            height: imageHeight, 
+            objectFit: 'cover', 
+            borderRadius: '0.5rem',
+            display: 'block',
+            opacity: isSoldOut ? '0.5' : '1'  // Dim the image for sold out products
+          }} 
+        />
+      )}
+  <div className="info">
+        <h3>{p.productName}</h3>
+        <div className="price">
+        {isSoldOut ? (
+    <div>
+      {['14', '15', '16', '23', '24', '25','34', '35', '36'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK120</span>
+      )}
+      {['20', '21', '22', '17', '18', '19', '37', '38', '39'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK110</span>
+      )}
+      {['12', '13', '33'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK100</span>
+      )}
+      {' '}SEK{displayPrice} (Sold Out)
+    </div>
+  ) : (
+    <div>
+      {['14', '15', '16', '23', '24', '25','34', '35', '36'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK120</span>
+      )}
+      {['20', '21', '22', '17', '18', '19', '37', '38', '39'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK110</span>
+      )}
+      {['12', '13', '33'].includes(p.productId.toString()) && (
+        <span style={{ textDecoration: 'line-through', color: '#FF6666' }}>SEK100</span>
+      )}
+      {' '}SEK{displayPrice}
+
+      </div>
+      
+
+    )}
+
         </div>
-      </button>
+      </div>
+    </button>
 
-      {modalShown && <ProductModal p={updatedProduct} image={image} setModalShown={setModalShown} />}
-    </>
-  );
+    {modalShown && <ProductModal p={updatedProduct} image={image} setModalShown={setModalShown} />}
+  </>
+);
 }
