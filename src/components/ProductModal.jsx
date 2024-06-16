474807828fe7c1642c71d78d126b6f2b5aaaf3d8
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-06-16 14:02:35 -0800

    went back to latest working code where i went back to the last change where you could also see quantity in my orders page not only in mail

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,127 +1,121 @@
-import { useAtom } from 'jotai';
-import { useState } from 'react';
-import { storeAtom } from '../lib/store';
-import { addToCart_localStorage } from '../lib/cart';
+import { useAtom } from 'jotai'
+import { useState } from 'react'
+import { storeAtom } from '../lib/store'
+import { addToCart_localStorage } from '../lib/cart'
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import CloseIcon from '@mui/icons-material/Close';
 import '../style/modal.css';
+// you can pass a state-setting-function as a prop
+// npm i jotai
+
 
 export default function ProductModal({ p, image, setModalShown }) {
-  const [store, setStore] = useAtom(storeAtom);
-  const [stagedItems, setStagedItems] = useState([]);
-  const [isAddToCartDisabled, setIsAddToCartDisabled] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [store, setStore] = useAtom(storeAtom)
+  const [stagedItems, setStagedItems] = useState([])
+
 
   const closeModal = () => {
     setModalShown(false);
   };
 
+
   function addToCart() {
+    // console.log(store.cart)
     setStore(current => {
-      current.cart.push(p);
-      addToCart_localStorage(p, store.loggedIn);
-      return { ...current };
-    });
+      current.cart.push(p)
+      addToCart_localStorage(p, store.loggedIn)
+      // return a copy of the current store, so react notices the change
+      return { ...current }
+    })
   }
 
-  function countInCart(productId) {
-    let count = store.cart.filter(item => item.productId === productId).length;
-    return count;
+  function removeFromCart() {
+    setStore(current => {
+      current.cart.splice(
+        current.cart.findIndex(i => i.productId === p.productId),
+        1
+      )
+      return { ...current }
+    })
+  }
+
+  function countInCart() {
+    // count the number of matching products
+    let count = store.cart.filter(item => {
+      return item.productName === p.productName
+    }).length
+    return count
   }
 
   function addToStagedItems() {
-    setStagedItems([...stagedItems, p]);
+    setStagedItems([...stagedItems, p])
   }
 
   function removeFromStagedItems() {
-    setStagedItems([...stagedItems.slice(0, stagedItems.length - 1)]);
+    setStagedItems([...stagedItems.slice(0, stagedItems.length - 1)])
   }
 
-  async function addStagedItemsToCart() {
-    // Calculate total items including staged items
-    const totalInCart = countInCart(p.productId) + stagedItems.length;
-
-
-
-    try {
-      // Make an API call to check product availability
-      const response = await fetch('https://api.vtscases.com/addToCartLimit', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'productId': p.productId, // Assuming p has a productId property
-          'count': totalInCart // Total items including staged items
-        }
-      });
-      console.log(p.productId)
-      console.log(totalInCart)
-
-      if (response.ok) {
-        // Add staged items to cart
-        for (let i = 0; i < stagedItems.length; i++) {
-          addToCart();
-        }
-        // Clear staged items and close modal
-        setStagedItems([]);
-        closeModal();
-      } else {
-        // Show error message
-        const result = await response.text(); // Assuming the response is plain text
-        setErrorMessage(result); // Set error message
-        setIsAddToCartDisabled(true); // Disable the add to cart button
-      }
-    } catch (error) {
-      console.error('Error adding product to cart:', error);
-      setErrorMessage("An error occurred while adding the product to the cart. Please try again later.");
+  function addStagedItemsToCart() {
+    for (let i = 0; i < stagedItems.length; i++) {
+      addToCart()
     }
+    setStagedItems([])
+    closeModal()
   }
 
-  const displayPrice = p.productPrice.toFixed(2);
-  let productType = p.productType.toLowerCase().split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  const displayPrice = p.productPrice.toFixed(2)
+  let producttype= p.productType;
+ 
 
-  return (
-    <div className="product-modal_backdrop" onClick={closeModal}>
-      <div className="product-modal" onClick={e => e.stopPropagation()}>
-        <div className="modal-header">
-          <h3>{p.productName}</h3>
-          <button onClick={closeModal}>
-            <CloseIcon />
-          </button>
-        </div>
-        <div className="modal-body">
-          <div className="left">
-            {image && <img className="product-modal_image" src={image} alt="" style={{ border: '1px solid #ccc', borderRadius: '0.5rem' }} />}
-          </div>
-          <div className="right">
-            <div>{productType}</div>
-            <div>Description: {p.productDescription}</div>
-            <div className="buttons-container">
-              <div className="buttons row">
-                <div className='col-3'>
-                  <button onClick={removeFromStagedItems} className='remove'>
-                    <RemoveIcon />
-                  </button>
-                </div>
-                <div className='col-3'>
-                  <button onClick={addToStagedItems} className='add'>
-                    <AddIcon />
-                  </button>
-                </div>
-                <div className='cart col-3'>
-                  <button disabled={stagedItems.length < 1 || isAddToCartDisabled} onClick={addStagedItemsToCart}>
-                    Add {stagedItems.length} to cart
-                  </button>
-                </div>
-                <div className='col-3'>
-                  <h4 className="price-mobile"> Kr{displayPrice}</h4>
+  // Convert the productType string to display in title case
+  let formattedProductType = producttype.toLowerCase()
+      .split('_')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  
+
+      return (
+        <div className="product-modal_backdrop" onClick={closeModal}>
+          <div className="product-modal" onClick={e => e.stopPropagation()}>
+            <div className="modal-header">
+              <h3>{p.productName}</h3>
+              <button onClick={closeModal}>
+                <CloseIcon />
+              </button>
+            </div>
+            <div className="modal-body">
+              <div className="left">
+                {image && <img className="product-modal_image" src={image} alt="" style={{ border: '1px solid #ccc', borderRadius: '0.5rem' }} />}
+              </div>
+              <div className="right">
+                <div>{formattedProductType}</div>
+                <div>Description: {p.productDescription}</div>
+                <div className="buttons-container">
+                  <div className="buttons row">
+                    <div className='col-3'>
+                      <button onClick={removeFromStagedItems} className='remove'>
+                        <RemoveIcon />
+                      </button>
+                    </div>
+                    <div className='col-3'>
+                      <button onClick={addToStagedItems} className='add'>
+                        <AddIcon />
+                      </button>
+                    </div>
+                    <div className='cart col-3'>
+                      <button disabled={stagedItems.length < 1} onClick={addStagedItemsToCart}>
+                        Add {stagedItems.length} to cart
+                      </button>
+                    </div>
+                    <div className='col-3'>
+                      <h4 className="price-mobile"> Kr{displayPrice}</h4>
+                    </div>
+                  </div>
                 </div>
               </div>
-              {errorMessage && <div className="error-message">{errorMessage}</div>}
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-}
+      );
+    }
