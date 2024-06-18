f0f86cfee7e2f66ba83f3c1ff360212033d10da9
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-06-18 12:33:58 -0800

    modified the text for mobile verisons and added the prevous price for the missing ids for both active and sold out cards

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     const [products, setProducts] = useState([])
     useEffect(()=>{
-        getProductsByIds([15, 18,12,13])
+        getProductsByIds([26,50,9,13])
             .then(p => {
                 setProducts(p)
             })
@@ -29,7 +29,7 @@ export default function Home() {
         <>
             <section id="hero">
                 <h2>VTS cases</h2>
-                <p className="mobile-text">Welcome to VTScases! Elevate your mobile experience with our stylish, protective phone cases. Choose from vibrant printed, timeless plain, or bold colored designs. Our cases combine fashion with functionality, offering trend-setting protection for your device!</p>
+                <p className="mobile-text">Welcome to VTScases! Elevate your mobile experience with our stylish, protective phone cases. Choose from silicone plain cases, colored silicone cases,printed silicone cases and printed premium rubber cases. Our cases combine fashion with functionality, offering trend-setting protection for your device!</p>
                 <p className="desktop-text">Welcome to VTScases, where style meets protection! Elevate your mobile experience with our diverse range of phone cases. At VTScases, we pride ourselves on offering a collection that caters to every taste. Whether you prefer the vibrant allure of printed cases, the timeless simplicity of plain designs, or the bold statement made by our colored cases, we have the perfect match for your style. Our cases not only reflect the latest trends but also provide top-notch protection for your device. Discover the fusion of fashion and functionality as you explore our curated selection. Shop VTScases today and let your phone make a statement!</p>
             </section>
             <section id="products">
