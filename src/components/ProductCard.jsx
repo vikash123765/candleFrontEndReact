1ae253b6ba7f137b7bbb857c0441517a935b643f
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-06-21 04:48:00 -0800

    updated stock based on new count of what is acceptable to sell, also removed complete products of which there was only one in stock as it will not be able to sel. deleted the imagemapping to reflect the actual stock and updated cart weight mapping removed unseceray ids

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ import Iphone15PlainCover from "../ProductImages/iphone 15 plain case silicone.j
 import a14premiumRubberPinkLotus from "../ProductImages/a14 premium rubber case pink lotus flower.jpg";
 import iphone15black from "../ProductImages/iphone 15 black silicone.jpg";
 
-import s23BlueMoon from "../ProductImages/blue moon sislicone case s23 ultra.jpg";
+
 import s23Blue from "../ProductImages/s23 ultra blue case sislicone.jpg";
 import s24PLain from "../ProductImages/s24 ultra plain case sislicone.jpg";
 import s23CrystalRosePremiumRubber from "../ProductImages/crystal rose red premium rubber samsung s23.jpg";
@@ -21,24 +21,17 @@ import iphone15RedEyeWorrior from "../ProductImages/iphone 15 pro premium case r
 import s23OceanMountain from "../ProductImages/s23 ultra premium rubber case ocean mountain.jpg";
 import a14black from "../ProductImages/black silicone case samsung a14 .jpg";
 import s23black from "../ProductImages/black s23 silicone case .jpg";
-import Iphone14proMaxBlue from "../ProductImages/iphone 14 pro max silicone blue.jpg";
-import iphone14promaxsamuraiLetters from "../ProductImages/iphone 14 pro max silicone case red moon samurai chinease letters.jpg";
+
 import s23UltraAstrounutMoon from "../ProductImages/s23 ultra sislicone astronaut moon.jpg";
 import s23Maldives from "../ProductImages/maldives s23  and s24 silicone case .jpg";
-import a14BlueMoon from "../ProductImages/a14 blue moon.png";
+
 import Iphone15BlueMoon from "../ProductImages/iphone15BlueMoon.png";
-import Iphone14proMaxBlueMoon from "../ProductImages/iphnew14promaxbluemoon.png";
-import iphone14ProMaxMoon from "../ProductImages/iphone14ProMaxMoon.png";
 
 import s23blumoon from "../ProductImages/s23 blue moon.png";
 import s23lion from "../ProductImages/s23lion.png";
-import a14lion from "../ProductImages/a14lion.png";
-import iphon15lion from "../ProductImages/iphon15lion.png";
-import iphone14promaxlion from "../ProductImages/iphone14promaxlion.png";
 
-import a14AstronautMoon from "../ProductImages/a14AstronautMoon.png";
-import iphone15AstronautMoon from "../ProductImages/iphone15AstronautMoon.png";
-import Iphone14promaxAstronautMoon from "../ProductImages/Iphone14promaxAstronautMoon.png";
+import iphon15lion from "../ProductImages/iphon15lion.png";
+
 
 import a14milkyway from "../ProductImages/a14milkyway.png";
 import iphone14promaxmilkyway from "../ProductImages/iphone14promax milky way.png";
@@ -47,9 +40,9 @@ import s23MilkyWay from "../ProductImages/s23MilkyWay.png";
 
 import iphone15TropicalIsland from "../ProductImages/iphone15TropicalIsland.png";
 import s23TropicalIsland from "../ProductImages/s23TropicalIsland.png";
-import a14tropicalIsland from "../ProductImages/a14tropicalIsland.png";
 
-import a14tajMahal from "../ProductImages/a14tajMahal.png";
+
+
 import iphone15tajmahal from "../ProductImages/iphone 15tajmahal.png";
 import s24tajmahal from "../ProductImages/s24tajmahal.png";
 
@@ -58,22 +51,20 @@ import iphone15waterfall from "../ProductImages/iphone15waterfall.png";
 import a14waterFall from "../ProductImages/a14waterFall.png";
 
 import a14Madlives from "../ProductImages/a14Madlives.png";
-import iphone15maldives from "../ProductImages/iphone 15 maldives.png";
-import iphone14ProMaxMaldives from "../ProductImages/iphone14ProMaxMaldives.png";
+
 
 import s23Mountainreflection from "../ProductImages/s23Mountainreflection.png";
 import a14mountainreflection from "../ProductImages/a14mountainreflection.png";
-import iphne14promaxmountainreflection from "../ProductImages/iphne14promaxmountainreflection.png";
 
-import iphone15redletterjapanse from "../ProductImages/iphone 15 red letter japanse.png";
-import s23redletterjap from "../ProductImages/s23 red letter jap.png";
+
+
 import moonEclipseamuraiIphone14ProMax from "../ProductImages/MoonEclipseamuraiIphone14ProMax.png";
 
 import iphone15Pinapple from "../ProductImages/iphone15Pinapple.png";
-import a14pinapple from "../ProductImages/a14pinapple.png";
+
 import iphone15Blue from "../ProductImages/iphone15Blue.png";
 
-iphone15Blue
+
 export default function ProductCard({p, isSoldOut}) {
   const [imageHeight, setImageHeight] = useState('300px'); // Default height for larger screens
   console.log("vikasproduscts", parseFloat)
@@ -86,7 +77,6 @@ export default function ProductCard({p, isSoldOut}) {
     11: Iphone15PlainCover, // Use the actual product ID here
     10: a14premiumRubberPinkLotus,
     12:iphone15black,
-    13: s23BlueMoon,
     14: s23Blue,
     15:s24PLain ,
     16:s23CrystalRosePremiumRubber,
@@ -101,45 +91,28 @@ export default function ProductCard({p, isSoldOut}) {
     25:s23OceanMountain,
     26:a14black,
     27:s23black,
-    28:Iphone14proMaxBlue,
-    29:iphone14promaxsamuraiLetters,
     30:s23UltraAstrounutMoon,
     31:s23Maldives,
-    32:a14BlueMoon,
     34:Iphone15BlueMoon,
-    35:Iphone14proMaxBlueMoon,
-    36:iphone14ProMaxMoon,
     72:s23blumoon,
     38:s23lion,
-    39:a14lion,
     40:iphon15lion,
-    41:iphone14promaxlion,
-    42:a14AstronautMoon,
-    43:iphone15AstronautMoon,
-    44:Iphone14promaxAstronautMoon,
     45:a14milkyway,
     46:iphone14promaxmilkyway,
     47:iphone15milkyway,
     48:s23MilkyWay,
     49:iphone15TropicalIsland,
     50:s23TropicalIsland,
-    52:a14tajMahal,
     53:iphone15tajmahal,
     54:s24tajmahal,
     55:s23WaterFall,
     56:iphone15waterfall,
     57:a14waterFall,
     58:a14Madlives,
-    59:iphone15maldives,
     60:s23Mountainreflection,
     61:a14mountainreflection,
-    62:iphne14promaxmountainreflection,
-    63:iphone15redletterjapanse,
-    64:s23redletterjap,
     65:moonEclipseamuraiIphone14ProMax,
     66:iphone15Pinapple,
-    67:a14pinapple,
-    68:iphone14ProMaxMaldives,
     71:iphone15Blue
 
 
